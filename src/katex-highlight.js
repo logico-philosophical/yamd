df3fd09e6e9e1e06487dd28621ad9bf1d4aa116b
logico-philosophical <ossia@korea.ac.kr>
2019-05-20 22:57:30 +0900

    Update src/katex-highlight.js

--- src/katex-highlight.js
+++ src/katex-highlight.js
@@ -2,9 +2,6 @@ var katex = require('katex');
 var hljs = require('highlight.js');
 var m42kup = require('./index');
 
-var {text, html} = m42kup.converter;
-var {escapeHtml, htmlFilter, pipe} = m42kup.converter.helper;
-
 var elements = {};
 
 elements['highlight'] = elements[';;;'] = r => {
@@ -13,36 +10,51 @@ elements['highlight'] = elements[';;;'] = r => {
 	}
 
 	var commonLangs = [
-		'apache', 'bash', 'coffeescript', 'cpp', 'cs', 'css', 'diff', 'http', 'ini', 'java', 'javascript', 'json', 'makefile', 'xml', 'markdown', 'nginx', 'objectivec', 'perl', 'php', 'python', 'ruby', 'sql'
+		'apache', 'bash', 'coffeescript', 'cpp', 'cs',
+		'css', 'diff', 'http', 'ini', 'java',
+		'javascript', 'json', 'makefile', 'xml', 'markdown',
+		'nginx', 'objectivec', 'perl', 'php', 'python',
+		'ruby', 'sql'
 	];
-	
-	return pipe(
-		r => text(r.text.replace(/(^[ \t]*\n)|(\n[ \t]*$)/g, '')),
-		r => html(hljs.highlightAuto(r.text, commonLangs).value),
-		r => html(`<pre class="hljs"><code>${r.html}\n</code></pre>`)
-	)(r);
+
+	var trimmed = r.text.replace(/(^[ \t]*\n)|(\n[ \t]*$)/g, ''),
+		highlighted = hljs.highlightAuto(trimmed, commonLangs).value;
+	return {
+		type: 'html',
+		html: `<pre class="hljs"><code>${highlighted}\n</code></pre>`
+	};
 };
 
 elements['math'] = elements['$'] = r => {
 	if (r.type != 'text') {
-		throw new TypeError('Non-text input');
+		throw new TypeError(`Non-text input`);
 	}
-	return html(katex.renderToString(r.text, {
+	var rendered = katex.renderToString(r.text, {
 		throwOnError: false,
 		displayMode: false,
 		strict: 'error'
-	}));
+	});
+
+	return {
+		type: 'html',
+		html: rendered
+	};
 };
 
 elements['displaymath'] = elements['$$'] = r => {
 	if (r.type != 'text') {
-		throw new TypeError('Non-text input');
+		throw new TypeError(`Non-text input`);
 	}
-	return html(katex.renderToString(r.text, {
+	var rendered = katex.renderToString(r.text, {
 		throwOnError: false,
 		displayMode: true,
 		strict: 'error'
-	}));
+	});
+
+	return {
+		type: 'html',
+		html: rendered
+	};
 };
 
 var options = {elements};
@@ -50,8 +62,6 @@ var options = {elements};
 var ret = {};
 
 for (var k in m42kup) ret[k] = m42kup[k];
-
-
 
 ret.renderMore = function renderMore(input) {
 	return m42kup.render(input, options);
