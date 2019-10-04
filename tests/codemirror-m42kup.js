17d3af287a8696a0aa34f5d2cb5b64f203a22741
logico-philosophical <ossia@korea.ac.kr>
2019-10-04 23:39:01 +0900

    Add CodeMirror

---
+++ tests/codemirror-m42kup.js
@@ -0,0 +1,82 @@
+(CodeMirror => {
+	function token(stream, state) {
+		if ((state.levels[state.levels.length - 1] || 0) < 0) {
+			var lvmLevel = -state.levels[state.levels.length - 1];
+			var rvmString = '>'.repeat(lvmLevel - 1) + '`';
+			if (stream.skipTo(rvmString)) {
+				stream.match(rvmString);
+				state.levels.pop();
+			} else stream.skipToEnd();
+
+			return 'comment';
+		}
+
+		var c = stream.peek();
+
+		if (c == '`') {
+			stream.next();
+
+			var level = 1;
+			while (c = stream.next()) {
+				if (c != '<') {
+					stream.backUp(1);
+					break;
+				}
+				level++;
+			}
+
+			state.levels.push(-level);
+
+			return 'comment';
+		} else if (c == '[') {
+			stream.next();
+
+			var level = 1;
+			while (c = stream.next()) {
+				if (c != '<') {
+					stream.backUp(1);
+					break;
+				}
+				level++;
+			}
+
+			var currentLevel = state.levels[state.levels.length - 1] || 0;
+			if (level < currentLevel) {
+				return null;
+			}
+
+			var tagNameRegex = /^(?:(?:\*{1,3}|={1,6}|\${1,2}|;{1,3}|[!"#$%&')*+,\-\/;=>?@\\^_{|}~]|[a-z][a-z0-9]*)|)/i;
+
+			stream.match(tagNameRegex);
+
+			var separatorRegex = /^(?:[.]|)/i;
+
+			stream.match(separatorRegex);
+
+			state.levels.push(level);
+
+			return 'tag';
+		} else if (c == '>' || c == ']') {
+			var lbmLevel = state.levels[state.levels.length - 1] || 0;
+			if (lbmLevel <= 0) return stream.next();
+
+			var rbmString = '>'.repeat(lbmLevel - 1) + ']';
+			if (stream.match(rbmString)) {
+				state.levels.pop();
+				return 'tag';
+			} else stream.next();
+		}  else /* none of '[', ']', '`', '>' */ {
+			// reduce text normalization overhead
+			stream.match(/^[^\[\]`>]+/);
+		}
+	}
+
+	CodeMirror.defineMode('m42kup', (config, parserConfig) => ({
+		startState(baseColumn) {
+			return {
+				levels: []
+			}
+		},
+		token
+	}));
+})(CodeMirror);
