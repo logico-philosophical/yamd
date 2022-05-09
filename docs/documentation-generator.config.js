9ed017d29732818930502c449e97b732529d1037
logico-philosophical (via Travis CI) <ossia@korea.ac.kr>
2022-05-09 15:46:24 +0000

    Deploy logico-philosophical/m42kup to github.com/logico-philosophical/m42kup.git:gh-pages

--- docs/documentation-generator.config.js
+++ docs/documentation-generator.config.js
@@ -8,11 +8,18 @@ function render(text) {
 	return m42kup.render(text);
 }
 
+var styles = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/highlight.js@10.7.3/styles/tomorrow.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/m42kup@latest/web/m42kup.default.css">`;
+
 module.exports = {
 	name: 'm42kup 설명서',
 	src: 'src',
 	dst: 'build',
 	render,
+	templateData: {
+		styles
+	},
 	list: [
 		{
 			name: '시작하기',
@@ -20,15 +27,15 @@ module.exports = {
 			list: [
 				{
 					name: 'm42kup 소개',
-					file: 'introduction'
+					file: 'introduction.m42kup'
 				},
 				{
 					name: '간단한 사용법',
-					file: 'basic-usage'
+					file: 'basic-usage.m42kup'
 				},
 				{
 					name: '설치',
-					file: 'installation'
+					file: 'installation.m42kup'
 				}
 			]
 		},
@@ -38,27 +45,27 @@ module.exports = {
 			list: [
 				{
 					name: '렌더링 옵션',
-					file: 'options'
+					file: 'options.m42kup'
 				},
 				{
 					name: '입출력 형식',
-					file: 'formats'
+					file: 'formats.m42kup'
 				},
 				{
 					name: 'm42kup',
-					file: 'm42kup'
+					file: 'm42kup.m42kup'
 				},
 				{
 					name: 'm42kup.parser',
-					file: 'm42kup.parser'
+					file: 'm42kup.parser.m42kup'
 				},
 				{
 					name: 'm42kup.renderer',
-					file: 'm42kup.renderer'
+					file: 'm42kup.renderer.m42kup'
 				},
 				{
 					name: 'm42kup.highlighter',
-					file: 'm42kup.highlighter'
+					file: 'm42kup.highlighter.m42kup'
 				}
 			]
 		}
